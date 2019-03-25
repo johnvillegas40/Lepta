a31d00132ccf2d3b03e315ccd4e354404eabee8b
John Villegas <jvillegas@foxwyn.com>
2019-03-25 10:51:35 -0400

    Removed unnecessary imports from index.

--- Lepta/index.js
+++ Lepta/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Navigation } from "react-native-navigation";
-import { Provider } from "react-redux";
 import startMainApp from "./App";
-import Placeholder from "./src/screens/Placeholder";
-import configureStore from "./src/store/configureStore";
 
 Navigation.events().registerAppLaunchedListener(() => startMainApp());
